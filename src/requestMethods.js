8376b111ca590d18535bd91058457a2a7d78ed8a
Poorvank Shah <poorvank.13may@gmail.com>
2023-01-01 22:26:01 +0530

    base url change

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://store-api.onrender.com/";
 
 // const TOKEN = () => {
 //   if (localStorage.getItem("persist:root") === null) { return null; }
